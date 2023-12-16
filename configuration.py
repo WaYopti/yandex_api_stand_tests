b34b085ac5b9587f9bbe37a0980ac5c1343c6c08
WaYopti <153213445+WaYopti@users.noreply.github.com>
2023-12-16 20:27:28 +0300

    первый коммит

---
+++ configuration.py
@@ -0,0 +1,8 @@
+URL_SERVICE = "https://ba800c93-5513-4664-a32f-5474e5a6e466.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+
+# для тренировки POST-запросов
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
